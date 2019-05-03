7b8abd48340cbaf2e2a2237cb7efe4ba18eea5d8
manoczar <mano.czar@gmail.com>
2019-05-03 09:04:55 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <p> {{ sayHello() }} - <a v-bind:href="link">Google</a> </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      title: 'Hello World!',
+      alternateTitle: 'Hello Manó!',
+      link: 'http://google.com'
+    }
+  },
+  methods: {
+    sayHello: function () {
+      return this.title
+    },
+    changeTitle: function () {
+      return this.alternateTitle
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
